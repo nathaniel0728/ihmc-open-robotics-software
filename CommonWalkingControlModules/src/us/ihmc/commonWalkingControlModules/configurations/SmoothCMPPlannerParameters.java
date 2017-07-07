1ab106b9f378ba9055b7bcbe616434e0e26e0eeb
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-07-07 15:31:16 -0500

    Switched to enumerated CoP waypoints. CoP and angular momentum planners not working

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/SmoothCMPPlannerParameters.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/SmoothCMPPlannerParameters.java
@@ -1,13 +1,38 @@
 package us.ihmc.commonWalkingControlModules.configurations;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
 
+import us.ihmc.euclid.geometry.BoundingBox2D;
 import us.ihmc.euclid.tuple2D.Vector2D;
 
 public class SmoothCMPPlannerParameters
 {
    private final double modelScale;
+   private final EnumMap<CoPPointName, Vector2D> copOffsetsFootFrame = new EnumMap<>(CoPPointName.class);
+   private final EnumMap<CoPPointName, Double> maxXCoPOffsets = new EnumMap<>(CoPPointName.class);
+   private final EnumMap<CoPPointName, Double> minXCoPOffsets = new EnumMap<>(CoPPointName.class);
+   private final EnumMap<CoPPointName, Double> maxYCoPOffsets = new EnumMap<>(CoPPointName.class);
+   private final EnumMap<CoPPointName, Double> minYCoPOffsets = new EnumMap<>(CoPPointName.class);
+
+   /**
+    * List of CoP points to plan in the order of planning
+    */
+   private final List<CoPPointName> copPointsToPlan = Arrays.asList(CoPPointName.HEEL_COP, CoPPointName.BALL_COP);
+   /**
+    * CoP offsets in foot frame
+    */
+   private final Vector2D[] copOffsets = {new Vector2D(0.0, -0.005), new Vector2D(0.0, 0.025)};
+   private final BoundingBox2D[] copOffsetLimits = {new BoundingBox2D(-0.04, -1, 0.03, 1), new BoundingBox2D(0.0, -1, 0.08, -1)};
+   /**
+    * Final CoP name (chicken support will be used only for this point)
+    */
+   private final CoPPointName endCoP = CoPPointName.MIDFEET_COP;
+   /**
+    * Percentage chicken support
+    */
+   private final double chickenSuppportPercentage = 0.5;
 
    public SmoothCMPPlannerParameters()
    {
@@ -31,44 +56,55 @@ public class SmoothCMPPlannerParameters
 
    public int getNumberOfWayPointsPerFoot()
    {
-      return 2;
+      return copPointsToPlan.size();
    }
 
-   public double[] getMaxCoPForwardOffsetsFootFrame()
+   public EnumMap<CoPPointName, Double> getMaxCoPForwardOffsetsFootFrame()
    {
-      double maxHeelOffset = modelScale * 0.03;
-      double maxBallOffset = modelScale * 0.08;
-
-      double[] maxCoPOffsets = { maxHeelOffset, maxBallOffset };
-
-      return maxCoPOffsets;
+      for (int i = 0; i < copPointsToPlan.size(); i++)
+      {
+         maxXCoPOffsets.put(copPointsToPlan.get(i), copOffsetLimits[i].getMaxX() * modelScale);
+      }
+      return maxXCoPOffsets;
    }
 
-   public double[] getMinCoPForwardOffsetsFootFrame()
+   public EnumMap<CoPPointName, Double> getMinCoPForwardOffsetsFootFrame()
    {
-      double minHeelOffset = modelScale * -0.04;
-      double minBallOffset = modelScale * 0.0;
-
-      double[] minCoPOffsets = { minHeelOffset, minBallOffset };
-
-      return minCoPOffsets;
+      for (int i = 0; i < copPointsToPlan.size(); i++)
+      {
+         minXCoPOffsets.put(copPointsToPlan.get(i), copOffsetLimits[i].getMinX() * modelScale);
+      }
+      return minXCoPOffsets;
    }
 
-   public List<Vector2D> getCoPOffsetsFootFrame()
+   public EnumMap<CoPPointName, Vector2D> getCoPOffsetsFootFrame()
    {
-      Vector2D heelOffset = new Vector2D(0.0, -0.005);
-      Vector2D ballOffset = new Vector2D(0.0, 0.025);
-
-      heelOffset.scale(modelScale);
-      ballOffset.scale(modelScale);
-
-      List<Vector2D> copOffsets = new ArrayList<>();
-      copOffsets.add(heelOffset);
-      copOffsets.add(ballOffset);
-
-      return copOffsets;
+      Vector2D tempVec;
+      for (int i = 0; i < copPointsToPlan.size(); i++)
+      {
+         tempVec = copOffsets[i];
+         tempVec.scale(modelScale);
+         copOffsetsFootFrame.put(copPointsToPlan.get(i), tempVec);
+      }
+      return copOffsetsFootFrame;
+   }
+   
+   public CoPPointName getEntryCoPName()
+   {
+      return copPointsToPlan.get(0);
+   }
+   
+   public CoPPointName getExitCoPName()
+   {
+      return copPointsToPlan.get(copPointsToPlan.size() -1);
    }
 
+   public List<CoPPointName> getCoPPointsToPlan()
+   {
+      return copPointsToPlan;
+   }
+   
+   // Need to understand what all of these do 
    public double getTransferSplitFraction()
    {
       return 0.5;
